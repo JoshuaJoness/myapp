bf2849ed9011bf3daba14cb4f4d39b6d75fad496
Joshua Jones <joshuajones@trentu.ca>
2020-02-29 16:53:15 -0500

    My first Angular project!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  name:string = 'Josh';
+
+	constructor() {
+		this.changeName('Jenilee')
+	}
+
+	changeName(name:string):void {
+		this.name = name
+	}
 }
---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service'
+import { Todo } from 'src/app/models/Todo'
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+	@Input() todo: Todo;
+	@Output() deleteTodo: EventEmitter<Todo> = new EventEmitter;
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit(): void {
+  }
+
+	// Set Dynamic Classes
+	setClasses() {
+		let classes = {
+			todo: true,
+			'is-complete': this.todo.completed
+		}
+
+		return classes
+	}
+
+	onToggle(todo) {
+		// toggle in UI
+		todo.completed = !todo.completed
+		// toggle on server
+		this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo))
+
+	}
+
+	onDelete(todo) {
+		// need to emit event upwards, here we will bring in EventEmitter & Output
+		this.deleteTodo.emit(todo)
+	}
+
+}
